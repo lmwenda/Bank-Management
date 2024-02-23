6b11d9631bd8351d0be60961268733a244749a65
lmwenda <lukemwen@hotmail.com>
2024-02-23 18:18:08 +0000

    solved storing data into struct

--- src/main.c
+++ src/main.c
@@ -1,32 +1,58 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "main.h"
+#include <string.h>
 
-char options[7][20] = {"View Menu", "New Account", "New Transaction", "View Transcations", "View Acccounts", "Edit Account"};
+char options[7][20] = {"View Menu", "New Account",    "New Transaction",
+                       "View Transcations", "View Acccounts", "Edit Account"};
 
 // creating new account
-void new_account()
-{
+void new_account() {
     printf("\n \n Initializing new account... \n");
     struct Account new_account;
 
     // prompt information
 
-    char[20] name;
-    int balance;
-    int debt;
-    int spent;
+    char *name = "sdfsdfsdfsdf";
     int phoneNumber;
-    char* address;
+    char *address = "sdfsdfsdfsf";
+
+    char *ptr_name = &name;
+    int *ptr_phoneNumber = &phoneNumber;
+    char *ptr_address = &address;
 
     // add the information to the new account created
 
-    
+    printf("\n Name: ");
+    scanf("%s", ptr_name);
+    printf("%s", ptr_name);
 
-}
+    printf("\n Phone Number: ");
+    scanf("%d", ptr_phoneNumber);
+    printf("%d", *ptr_phoneNumber);
 
-// menu options initialization
+    printf("\n Address: ");
+    scanf("%s", ptr_address);
+    printf("%s", ptr_address);
+
+    // store the information in the new account struct
+
+    printf("\n Parsing the information into the new account... \n");
+
+    new_account.name = ptr_name;
+    new_account.phoneNumber = *ptr_phoneNumber;
+    new_account.address = ptr_address;
+
+    new_account.balance = 0;
+    new_account.debt = 0;
+    new_account.spent = 0;
+
+    printf("New Account created: %s(#%d) \n", new_account.name, new_account.phoneNumber);
+  }
+
+// a conditional statement to find the right function for their option
+// e.g 1 -> new_account();
 void menu_opts_init(int c_opt)
 {
     switch(*&c_opt)
@@ -43,87 +69,96 @@ void menu_opts_init(int c_opt)
 }
 
 // toggle view menu
-int tv_menu(char options[7][20])
-{
-    for (register int i = 0; i<6; i++)
-        printf("\n Option %d: %s", i, options[i]);
+int tv_menu(char options[7][20]) {
+  for (register int i = 0; i < 6; i++)
+    printf("\n Option %d: %s", i, options[i]);
 
+  printf(
+      "\n \n What would you like to do? \n Pick an option by their number: ");
+  int c_opt; // chosen option integer
+  scanf("%d", &c_opt);
+  printf("\n You've chosen: %s \n", options[*&c_opt]);
 
-    printf("\n \n What would you like to do? \n Pick an option by their number: ");
-    int c_opt; // chosen option integer
-    scanf("%d", &c_opt);
-    printf("\n You've chosen: %s \n", options[*&c_opt]);
+  menu_opts_init(c_opt);
 
-    menu_opts_init(c_opt);
-    
-    return *&c_opt;
+  return *&c_opt;
 }
 
 // menu intialization
-void menu_init()
-{
-    printf("Welcome to Bank of Atmora \n");
+void menu_init() {
+  printf("Welcome to Bank of Atmora \n");
 
-    const int c_opt = tv_menu(options);
-
-
+  const int c_opt = tv_menu(options);
 }
 
-// void function of me practising pointers, memory address, allocating memory & managing time 
-// function filled with memory junk experiments
-void mem_junk_init()
-{
-    const int value = 100;
-    const int* avalue = &value;
+// void function of me practising pointers, memory address, allocating memory &
+// managing time function filled with memory junk experiments
+void mem_junk_init() {
+  const int value = 100;
+  const int *avalue = &value;
 
-    printf("\n value: %d", *avalue);
-    printf("\n address of value: %p", avalue);
+  printf("\n value: %d", *avalue);
+  printf("\n address of value: %p", avalue);
 
+  int *arrval;
+  arrval = (int *)malloc(sizeof(int));
+  *arrval = 500;
 
-    int* arrval;
-    arrval = (int*)malloc(sizeof(int));
-    *arrval = 500;
+  int *array;
+  array = malloc(*arrval * sizeof(int));
 
-    int* array;
-    array = malloc(*arrval * sizeof(int));
+  int total = 0;
+  int *ptr = &total;
+  for (int i = 0; i < *arrval; i++) {
+    array[i] = i + 1;
+    printf("\n %d", array[i]);
+    *ptr = *ptr + 1;
+  }
+  free(arrval);
+  free(array);
 
+  printf("\n total: %d", *ptr);
 
-    int total = 0;
-    int *ptr = &total;
-    for (int i = 0; i < *arrval; i++)
-    {
-        array[i] = i + 1;
-        printf("\n %d", array[i]);
-        *ptr = *ptr + 1;
-    }
-    free(arrval);
-    free(array);
-    
-    printf("\n total: %d", *ptr);
-
-    const char* name = "Create a New Account";
-    printf("\n size of memory allocated: %ld", sizeof(name));
+  const char *name = "Create a New Account";
+  printf("\n size of memory allocated: %ld", sizeof(name));
 }
 
+// Execution time: 0.000128 seconds
+void ptr_benchmark()
+{
+  int i = 5;
+  int* ptr = &i;
+  printf("\n %d", *ptr);
+}
+
+// Execution time: 0.000035 seconds
+void int_benchmark()
+{
+  int i = 5;
+  printf("\n %d", i);
+}
 
 // main function
-int main(int argc, char const *argv[])
-{   
-    clock_t start, end;
+int main(int argc, char const *argv[]) {
+  clock_t start, end;
 
-    double cpu_time_used;
+  double cpu_time_used;
 
-    start = clock();
+  start = clock();
 
-    // code to measure
-    menu_init();
+  // code to measure
 
+  menu_init();
 
-    end = clock();
+    // mem_junk_init();
+    // ptr_benchmark();
+    // int_benchmark();
 
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+  end = clock();
 
-    printf("Execution time: %f seconds\n", cpu_time_used);
+  cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
 
-    return 0;
+  printf("Execution time: %f seconds\n", cpu_time_used);
+
+  return 0;
 }
--- src/main.h
+++ src/main.h
@@ -1,28 +1,25 @@
-#pragma once 
+#pragma once
 
-struct Transaction
-{
-    int fromId;
-    int toId;
+struct Transaction {
+  int fromId;
+  int toId;
 
-    int amount;
-    int fromBalance;
+  int amount;
+  int fromBalance;
 };
 
-struct Account
-{
-    /* data */
-    int id;
-    char name[24];
-    int balance; // grand total balance
-    int debt; // how much they owe
-    int spent; // how much they spent
-    
-    int phoneNumber;
-    char* address;
+struct Account {
+  /* data */
+  int id;
+  char *name;
+  int balance; // grand total balance
+  int debt;    // how much they owe
+  int spent;   // how much they spent
+
+  int phoneNumber;
+  char *address;
 };
 
-char options[7][20];
 void new_account();
 void menu_opts_init(int c_opt);
 int tv_menu(char options[7][20]);
